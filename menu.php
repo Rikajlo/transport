2bb15e73474d760522f1d817097521123ae227b4
Martin <martin.kis.98@gmail.com>
2018-12-20 17:25:59 +0100

    Revert "menija"
    This reverts commit 6f6f648d8ef1e05e869fd1c1a3c0f5f248c28bec.

--- menu.php
+++ menu.php
@@ -1,7 +1,5 @@
 <?php
 
-@session_start();
-
 echo '
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -19,25 +17,10 @@ echo '
       </li>
       <li class="nav-item">
         <a class="nav-link" href="news.php">News</a>
-         </li>
-         <li class="nav-item">
-        <a class="nav-link" href="#">Taxi</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Rent a Bike</a>
-      </li>';
-if(isset($_SESSION['ID_User'])){
-echo'
-      <li class="nav-item">
-        <a class="nav-link" href="logout.php">Log out</a>
-      </li>}'; }
-      else {
-    echo'
-      <li class="nav-item">
-        <a class="nav-link" href="login.php">Log in</a>
-      </li>';
-      }
-      echo'
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
     </ul>
   </div>
 </nav>
